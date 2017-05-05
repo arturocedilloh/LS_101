ca096b7eb06cb53dc70e838db4c435d148c0ac9f
Arturo Cedillo <arturo.cedillo.h@live.com>
2017-05-05 22:56:54 +1000

    Lesson 6 - Tewnty one - 170505

--- lesson_6/test.rb
+++ lesson_6/test.rb
@@ -1,38 +1,50 @@
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
-WINNING_LINES = [[1, 2, 3]]              # diagonal
-require 'pry'
-bord = ['X', 'X',' ']
-
-
-
-def computer_places_piece!(brd)
-
+=begin
+def card_count(cards)
+  total = 0
+  cards.each do |card|
+    if %[king queen jack].include? card
+      total += 10
+    else
+      total += card.to_i
+    end
+  end
+  total
 end
 
-p bord
-p computer_places_piece!(bord)
-p bord
+cards = [["s", "king"], ["h", "ace"]]
+values = cards.map {|card| card[1]}
+ace_cards = values.select {|i| i == "ace"}
+non_ace_cards = values.select {|i| i != "ace"}
 
+p non_ace_cards
 
-
-=begin
-def test (brd)
-  move = 0
-brd.each_with_index do |item, index|
-  if item == INITIAL_MARKER
-    move = index
-  else
-    item
+total_count = card_count(non_ace_cards)
+if ace_cards != []
+  ace_cards.count.times do
+    if total_count < 11
+      total_count += 11
+    else
+      total_count += 1
+    end
   end
 end
-brd[move] = COMPUTER_MARKER
 
+
+p total_count
+
+=end
+def display_board(player, dealer)
+  system "clear" # clears the screen
+  puts ""
+  puts "  Dealers Cards  "
+  puts "                 "
+  puts "     #{dealer}"
+  puts "-----------------------------------------"
+  puts "  Player Cards  "
+  puts "                 "
+  puts "     #{player}"
+  puts "-------------------------------------------"
 end
 
-p bord
-p test(bord)
-p bord
-=end
+display_board([["S", "9"], ["H", "A"]], [["H", "1"], ["H", "7"], ["H", "A"]])
--- lesson_6/tewnty_one.rb
+++ lesson_6/tewnty_one.rb
@@ -10,32 +10,117 @@
 
 player_cards = []
 dealer_cards = []
-deck_cards =[]
+
+# rubocop:disable Metrics/AbcSize
+def display_board(player, dealer)
+  system "clear" # clears the screen
+  puts ""
+  puts "  Dealers Cards  "
+  puts "                 "
+  puts "     #{dealer}"
+  puts "-----------------------------------------"
+  puts "  Player Cards  "
+  puts "                 "
+  puts "     #{player}"
+  puts "-------------------------------------------"
+end
+# rubocop:enable Metrics/AbcSize
 
 def deck_type(deck, card)
-  deck << ['h',card]
-  deck << ['d',card]
-  deck << ['c',card]
-  deck << ['s',card]
+  deck << ['h', card]
+  deck << ['d', card]
+  deck << ['c', card]
+  deck << ['s', card]
 end
 
-def initialize_deck (deck)
+def initialize_deck(deck)
   (0..12).each do |i|
     case i
-    when 0 then deck_type(deck, "ace")
-    when 10 then deck_type(deck, "jack")
-    when 11 then deck_type(deck, "queen")
-    when 12 then deck_type(deck, "king")
+    when 0 then deck_type(deck, "A")
+    when 10 then deck_type(deck, "J")
+    when 11 then deck_type(deck, "Q")
+    when 12 then deck_type(deck, "K")
     else
-      deck << ['h',"#{i + 1}"]
-      deck << ['d',"#{i + 1}"]
-      deck << ['c',"#{i + 1}"]
-      deck << ['s',"#{i + 1}"]
+      value = (i + 1).to_s
+      deck << ['H', value]
+      deck << ['D', value]
+      deck << ['C', value]
+      deck << ['S', value]
     end
   end
 
   deck.shuffle!
 end
 
-initialize_deck(deck_cards)
+def no_ace_card_count(cards)
+  total = 0
+  cards.each do |card|
+    if %(K Q J).include? card
+      total += 10
+    else
+      total += card.to_i
+    end
+  end
+  total
+end
+
+def ace_card_count(cards, current_total)
+  cards.count.times do
+    if current_total < 11
+      current_total += 11
+    else
+      current_total += 1
+    end
+  end
+  current_total
+end
+
+def total_deck_count(cards)
+  values = cards.map { |card| card[1] }
+  ace_cards = values.select { |i| i == "A" }
+  non_ace_cards = values.select { |i| i != "A" }
+
+  non_ace_count = no_ace_card_count(non_ace_cards)
+  total_count = ace_card_count(ace_cards, non_ace_count)
+end
+
+
+loop
+  # initialize_deck(deck_cards)
+  dealer_cards.push(deck_cards.pop)
+  player_cards.push(deck_cards.pop)
+  player_cards.push(deck_cards.pop)
+  display_board(player_cards,dealer_cards)
+  player_deck_count = 0
+  dealer_deck_count = 0
+  answer = nil
+  loop do
+    puts "hit or stay?"
+    answer = gets.chomp
+    break if answer == 'stay' || busted?
+    player_cards.push(deck_cards.pop)
+    player_deck_count = total_deck_count(player_cards)
+    display_board(player_cards,dealer_cards)
+  end
+
+  if busted?
+    # end game or ask user to play again
+  else
+    puts "You chose to stay!"
+  end
+
+  while dealer_deck_count < player_deck_count
+    dealer_cards.push(deck_cards.pop)
+    dealer_deck_count = total_deck_count(dealer_cards)
+    display_board(player_cards,dealer_cards)
+    break if dealer_deck_count > 17 || busted?
+
+    if busted?
+      # end game or ask user to play again
+    end
+  end
+end
+
 p deck_cards
+
+p total_count
